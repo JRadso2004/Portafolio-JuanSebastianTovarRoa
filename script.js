ffe1fb25dfba6aaa69dd0d496de9b210eaff49dc
JUMA <jstovar87414@soy.sena.edu.co>
2023-12-04 21:46:40 -0500

    actualizacion de css

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ function toggleMenu() {
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
-  }
+}
 
 
-  function scrollToSection(contact) {
+function scrollToSection(contact) {
     var section = document.getElementById(contact);
     if (section) {
         section.scrollIntoView({ behavior: 'smooth' });
